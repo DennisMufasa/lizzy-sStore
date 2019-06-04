9c490d43ee6b6362392ed2422d939104c49129a3
DennisMufasa <denny.muasa@gmail.com>
2019-06-04 20:31:03 +0300

    chore refactore application to develop backend

--- add_product.php
+++ add_product.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,7 +15,44 @@
 
 </head>
 <body style="background: rgb(235, 229, 211);">
-    
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a class="navbar-brand" href="#"><span style="color: orange;">Lizzy's Store</span></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+              
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Inventory</a>
+                    </li>
+                    <li class="nav-item">
+                            <a class="nav-link" href="#" >Sales</a>
+                          </li>
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        more
+                      </a>
+                      <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="add_product.php">add product</a>
+                        <a class="dropdown-item" href="delete.php">delete product</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="update_cost.php">update product cost</a>
+                        <a class="dropdown-item" href="update_category.php">update product category</a>
+                      </div>
+                    </li>
+                    
+                  </ul>
+                  <form class="form-inline my-2 my-lg-0" action="" method="GET">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                  </form>
+                </div>
+              </nav>    
+
     <div class="container">
         <center style="margin-top: 12%; width: 80%; margin-left:10%;">
             <form class="border" action="" method="POST">
--- sale.php
+++ sale.php
@@ -1,21 +1,32 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Lizzy's</title>
+    <title></title>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+<style>
+    table{
+        margin-top: 10%;
+        width: 80%;
+        margin-left: 10%;
+    }
+    button{
+        background: rgb(121, 158, 228);
+    }
+</style>
 
 </head>
 <body>
-    
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <a class="navbar-brand" href="#"><span style="color: orange;">Lizzy's Store</span></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                   <span class="navbar-toggler-icon"></span>
@@ -37,11 +48,11 @@
                         more
                       </a>
                       <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">add product</a>
-                        <a class="dropdown-item" href="#">delete product</a>
+                        <a class="dropdown-item" href="add_product.php">add product</a>
+                        <a class="dropdown-item" href="delete.php">delete product</a>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">update product cost</a>
-                        <a class="dropdown-item" href="#">update product category</a>
+                        <a class="dropdown-item" href="update_cost.php">update product cost</a>
+                        <a class="dropdown-item" href="update_category.php">update product category</a>
                       </div>
                     </li>
                     
@@ -52,5 +63,27 @@
                   </form>
                 </div>
               </nav>
+
+    <table border="3">
+        <tr>
+            <th>product name</th>
+            <th>product category</th>
+            <th>product cost</th>
+            <th>quantity</th>
+        </tr>
+        <tr>
+            <td>Chealsey boots</td>
+            <td>Mens shoes</td>
+            <td>4500</td>
+            <td>
+                <form action="" method="GET">
+                    
+                    <center>
+                        <input type="number" name="qty" placeholder="enter quantity" required><br><br>
+                        <button type="submit" name="submit">Buy</button></center>
+                </form>
+            </td>
+        </tr>
+    </table>
 </body>
 </html>
